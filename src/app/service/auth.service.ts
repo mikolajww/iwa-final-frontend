03c725a63547da4ca0eba3a913afe1ecbcb66113
mikolaj <iwa-4@sr.dmcs.pl>
2018-06-16 18:24:11 +0100

    added redirect after successful signup

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {p} from "@angular/core/src/render3";
 
 @Injectable()
 export class AuthService {
