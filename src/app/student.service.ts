909e32e8637f14dce0054a8f7bf09f9a931ded9f
mikolaj <iwa-4@sr.dmcs.pl>
2018-06-16 17:32:12 +0100

    Added search component functionality through the service

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Student} from './student';
 import {Grade} from './grade';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpHandler, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs/Observable";
 import {tap} from "rxjs/operators";
 
@@ -23,7 +23,46 @@ export class StudentService {
   private filter:string = "";
   constructor(private http:HttpClient) { }
 
-  sortStudents() {
+  sortStudents(students:Student[]):Student[] {
+    return students.sort((s1,s2) => {
+      if(this.sortingParam == SortingParameter.NAME) {
+        if(this.sortingType == SortingType.DESC) {
+          if(s1.name < s2.name) return 1;
+          else if (s1.name > s2.name) return -1;
+          else return 0;
+        }
+        else {
+          if(s1.name > s2.name) return 1;
+          else if (s1.name < s2.name) return -1;
+          else return 0;
+        }
+      }
+      else if(this.sortingParam == SortingParameter.SURNAME) {
+        if(this.sortingType == SortingType.DESC) {
+          if(s1.surname < s2.surname) return 1;
+          else if (s1.surname > s2.surname) return -1;
+          else return 0;
+        }
+        else {
+          if(s1.surname > s2.surname) return 1;
+          else if (s1.surname < s2.surname) return -1;
+          else return 0;
+        }
+      }
+      else {
+        if(this.sortingType == SortingType.DESC) {
+          if(s1.indexNr < s2.indexNr) return 1;
+          else if (s1.indexNr > s2.indexNr) return -1;
+          else return 0;
+        }
+        else {
+          if(s1.indexNr > s2.indexNr) return 1;
+          else if (s1.indexNr < s2.indexNr) return -1;
+          else return 0;
+        }
+      }
+    })
+
   }
 
   getStudents() : Observable<Student[]> {
