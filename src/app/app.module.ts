38bf15a0bbe8297edc648e7685c7171b95524186
Revison <iwa-5@sr.dmcs.pl>
2018-06-16 17:32:11 +0200

    basic router support added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { StudentListComponent } from './student-list/student-list.component';
 import { StudentDetailComponent } from './student-detail/student-detail.component';
-import { StudentService } from './student.service';
+import {StudentService} from './student.service';
 import { StudentSearchComponent } from './student-search/student-search.component';
 import { FilterPipe } from './filter.pipe';
 import { StudentAddComponent } from './student-add/student-add.component';
 import { HttpClientModule } from "@angular/common/http";
+import { AboutComponent } from './about/about.component';
+import {RouterModule, Routes} from "@angular/router";
 
-
+const routes:Routes = [
+  {path: '',redirectTo:'home', pathMatch:'full'},
+  {path: 'home', component:StudentListComponent},
+  {path: 'about', component:AboutComponent}
+]
 
 @NgModule({
   declarations: [
@@ -19,11 +26,13 @@ import { HttpClientModule } from "@angular/common/http";
     StudentDetailComponent,
     StudentSearchComponent,
     FilterPipe,
-    StudentAddComponent
+    StudentAddComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [StudentService],
   bootstrap: [AppComponent]
